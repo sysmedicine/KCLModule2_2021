8202e04ecd419b9926874a2e2bc41da49feb93d0
Muhammad Arif <arif2@kth.se>
2021-09-23 12:12:42 +0200

    Update Day0_InstallingPackages.R

--- MetagenomicsWS/Day0_InstallingPackages.R
+++ MetagenomicsWS/Day0_InstallingPackages.R
@@ -1,4 +1,4 @@
-install.packages(c('readxl', 'tidyverse', 'dplyr', 'vegan'))
+install.packages(c('readxl', 'tidyverse', 'dplyr', 'vegan', 'ggpubr'))
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install(c('phyloseq', 'microbiome'))
